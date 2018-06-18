be879f40a8ff50745bbadc4338c89bb032fff6f0
DavidCdeB <davidcarrascobustur@gmail.com>
2018-06-18 11:03:32 +0100

    improve

--- run.sh
+++ run.sh
@@ -1,16 +1,17 @@
 #
 Scriptdir=`pwd`
 
-rm -Rf ./results/supercells_integers_0_1_-1_2_-2/tol_1_equal_to_10
-mkdir -p  ./results/supercells_integers_0_1_-1_2_-2/tol_1_equal_to_10
+rm -Rf ./results/supercells_integers_0_1_-1/tol_1_equal_to_10
+mkdir -p  ./results/supercells_integers_0_1_-1/tol_1_equal_to_10
 
-cd ./results/supercells_integers_0_1_-1_2_-2/tol_1_equal_to_10
+cd ./results/supercells_integers_0_1_-1/tol_1_equal_to_10
 
 Workdir=`pwd`
 
 cd $Scriptdir
 
 cp supercell_generator_v2.py $Workdir
+cp *out $Workdir
 
 
 FILES="
@@ -41,6 +42,7 @@ for i in ${FILES}; do
 cd $Workdir
 
 cp supercell_generator_v2.py ./${i}
+cp *out ./${i}
 cd ${i} 
 
 sed -i s/tol_2=1E-5/tol_2=1E-${i}/ supercell_generator_v2.py
